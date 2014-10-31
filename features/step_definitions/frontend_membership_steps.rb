3ac6f484892b265bc03adc989f43ea26bd26ad85
WMDE-Fisch <christoph.fischer@wikimedia.de>
2014-10-31 13:09:03 +0100

    minor refactoring

--- features/step_definitions/frontend_membership_steps.rb
+++ features/step_definitions/frontend_membership_steps.rb
@@ -2,20 +2,21 @@
 # @author Christoph Fischer <christoph.fischer@wikimedia.de>
 
 Then(/^The (private|business) membership address data should be the same$/) do | address_type |
+  page = FrontendReceiptPage
   if address_type == 'private'
-    expect(on(FrontendReceiptPage).input_first_name_element.value).to be == @address_data['first-name']
-    expect(on(FrontendReceiptPage).input_last_name_element.value).to be == @address_data['last-name']
+    expect(on(page).input_first_name_element.value).to be == @address_data['first-name']
+    expect(on(page).input_last_name_element.value).to be == @address_data['last-name']
   else
-    expect(on(FrontendReceiptPage).input_company_name_element.value).to be == @address_data['company-name']
+    expect(on(page).input_company_name_element.value).to be == @address_data['company-name']
   end
-  expect(on(FrontendReceiptPage).input_street_element.value).to be == @address_data['street']
-  expect(on(FrontendReceiptPage).input_post_code_element.value).to be == "#{@address_data['post-code']}"
-  expect(on(FrontendReceiptPage).input_city_element.value).to be == @address_data['city']
-  expect(on(FrontendReceiptPage).input_email_element.value).to be == @address_data['email']
+  expect(on(page).input_street_element.value).to be == @address_data['street']
+  expect(on(page).input_post_code_element.value).to be == "#{@address_data['post-code']}"
+  expect(on(page).input_city_element.value).to be == @address_data['city']
+  expect(on(page).input_email_element.value).to be == @address_data['email']
 end
 
 And(/^The lower fee rates should be (available|unavailable)$/) do | lower_fee |
-  if (lower_fee == 'available')
+  if lower_fee == 'available'
     is_disabled = nil
     on(FrontendReceiptPage) do | page |
       page.wait_until do
@@ -23,7 +24,7 @@ And(/^The lower fee rates should be (available|unavailable)$/) do | lower_fee |
       end
     end
   end
-  if (lower_fee == 'unavailable')
+  if lower_fee == 'unavailable'
     is_disabled = 'true'
     on(FrontendReceiptPage) do | page |
       page.wait_until do
