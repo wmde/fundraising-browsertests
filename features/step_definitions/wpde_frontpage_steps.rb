48a8bbf6a06db1b82404483e0c83006c62360a92
WMDE-Fisch <christoph.fischer@wikimedia.de>
2014-11-21 11:20:03 +0100

    added initial wp.de steps

---
+++ features/step_definitions/wpde_frontpage_steps.rb
@@ -0,0 +1,53 @@
+Given(/^I am on the wikipedia\.de frontpage$/) do
+	visit(WikipediaDeFrontPage)
+end
+
+And(/^The wikipedia\.de lightbox banner shows$/) do
+	on(WikipediaDeFrontPage) do | page |
+		page.wait_until do
+			page.form_donation_element.exists?
+		end
+	end
+end
+
+When(/^I click on the debit button$/) do
+	on(WikipediaDeFrontPage).button_debit_element.click
+end
+
+When(/^I click on the deposit button$/) do
+	on(WikipediaDeFrontPage).button_deposit_element.click
+end
+
+When(/^I click on the credit button$/) do
+	on(WikipediaDeFrontPage).button_credit_element.click
+end
+
+When(/^I click on the paypal button$/) do
+	on(WikipediaDeFrontPage).button_paypal_element.click
+end
+
+When(/^I click the banner (.*) amount option$/) do | amount |
+	on(WikipediaDeFrontPage).click_banner_amount(amount)
+end
+
+def generate_random_amount
+	random = Random.new
+	random.rand(1..99999)
+end
+
+And(/^I enter an random valid amount$/) do
+	@amount = generate_random_amount
+	on(WikipediaDeFrontPage).input_amount = @amount
+end
+
+And(/^The given amount should show$/) do
+	expect(on(FrontendFrontPage).get_donation_amount_element.text).to be == "#{@amount}€"
+end
+
+Then(/^The donation lightbox shows$/) do
+	on(WikipediaDeFrontPage) do | page |
+		page.wait_until do
+			page.form_donation_element.visible?
+		end
+	end
+end
