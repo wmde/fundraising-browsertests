c42604f3546e24891e6bed26f3cda6d0f58c442e
Leszek Manicki <leszek.manicki@wikimedia.de>
2015-09-11 10:00:09 +0200

    Update Credit Card Donation Page definition after changes in the UI

--- features/support/pages/frontend_creditpage.rb
+++ features/support/pages/frontend_creditpage.rb
@@ -4,11 +4,9 @@
 class FrontendCreditPage
   include PageObject
 
-  in_iframe(id: 'spenden-portal') do |outer_frame|
-    in_iframe({ id: 'micropayment-portal' }, outer_frame) do |inner_frame|
-      text_field(:input_holder, id: 'holder', frame: inner_frame)
-      text_field(:input_card_number, id: 'card-number', frame: inner_frame)
-      text_field(:input_cvc_code, id: 'cvc-code', frame: inner_frame)
-    end
+  in_iframe({ id: 'micropayment-portal' }) do |mcp_frame|
+    text_field(:input_holder, id: 'holder', frame: mcp_frame)
+    text_field(:input_card_number, id: 'card-number', frame: mcp_frame)
+    text_field(:input_cvc_code, id: 'cvc-code', frame: mcp_frame)
   end
 end
